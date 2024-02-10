96254e3afc36491ef6f4a6d9440f1ee6e8e691a5
Abdul-2Rehman <abdulrehman51423@gmail.com>
2024-02-10 14:53:31 +0500

    minor change

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import { app } from "./index.js";
 
 const PORT = process.env.PORT || 4000;
 
+app.get("/", (req, res) => {
+  res.send("Optics-Management-System Api Successfully Worked");
+});
 app.listen(PORT, () => {
   dbConnection();
   console.log("Server Connected On Port: " + PORT);
